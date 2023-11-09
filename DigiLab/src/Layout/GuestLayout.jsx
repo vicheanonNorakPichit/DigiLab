13be1bc99c393decef743368ee28245988ab3e7d
Chanven-Sok <chanven.sok@student.cadt.edu.kh>
2023-11-09 16:24:56 +0700

    add footer

--- DigiLab/src/Layout/GuestLayout.jsx
+++ DigiLab/src/Layout/GuestLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../Component/Navbar";
+import Footer from "../Component/Footer";
 
 const GuestLayout = () => {
   return (
     <div className="font-poppins">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
